3901b144cbdf1323aec3d4cfac7fbb0eea67452d
Santosh Yadav <santoshgkp107@gmail.com>
2025-04-15 21:43:29 +0530

    Update basic.c

--- Recursion/basic.c
+++ Recursion/basic.c
@@ -1,16 +1,12 @@
 
 #include <stdio.h>
-
 int fun(int n)//declered function
-{
-	if(n == 1)
-		return 0;
+{if(n == 1)
+	return 0;
 	else
-		return 1 + fun(n / 2);
+       return 1 + fun(n / 2);
 }
 int main() {
-	
 	printf("%d",fun(16));//rection
-	
 	return 0;
 }
